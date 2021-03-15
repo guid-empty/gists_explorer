b96a42f5d1a86170c7f0b9a8a94e413f0f00eb52
andrey.smirnov <asmirnov@inspector-cloud.com>
2021-03-16 02:04:23 +0300

    initial

--- gists_explorer/bin/main.dart
+++ gists_explorer/bin/main.dart
@@ -30,10 +30,62 @@ Future initiate() async {
     print(count);
   }
 
-  final currentPagesCountElement = document.querySelector('div[role=\'navigation\'] > em[data-total-pages]');
+  final currentPagesCountElement =
+      document.querySelector('div[role=\'navigation\'] > em[data-total-pages]');
   if (currentPagesCountElement != null) {
-    final pages = int.tryParse(currentPagesCountElement.attributes['data-total-pages']);
+    final pages =
+        int.tryParse(currentPagesCountElement.attributes['data-total-pages']);
     print(pages);
   }
 
+  final snippetElements = document.querySelectorAll('div.gist-snippet');
+  for (final snippetElement in snippetElements) {
+    ///
+    /// meta
+    ///
+    final metaElement = snippetElement.querySelector('div.gist-snippet-meta');
+    final authorElement = metaElement.querySelector(
+        'div > div.px-lg-2 > span > a[data-hovercard-type=\'user\']');
+    final author = authorElement.text.trim();
+    final avatarUrl = metaElement.querySelector('img.avatar-user').attributes['src'];
+    final fileNameElement =
+        metaElement.querySelector('strong.css-truncate-target');
+    final fileName = fileNameElement.text.trim();
+    final gistId = fileNameElement.parent.attributes['href'];
+    final timeAgo = metaElement.querySelector('time-ago').text.trim();
+    final tagElements =
+        metaElement.querySelectorAll('ul > li.d-inline-block > a.Link--muted');
+    final filesCount = tagElements.first.text.trim();
+    final forksCount = tagElements[1].text.trim();
+    final commentsCount = tagElements[2].text.trim();
+    final starsCount = tagElements.last.text.trim();
+
+    final gistNameElement =
+        metaElement.querySelector('div > div.px-lg-2 > span.f6');
+    final gistName = gistNameElement != null ? gistNameElement.text.trim() : '';
+
+    print('_' * 60);
+    print('author -> $author');
+    print('avatarUrl -> $avatarUrl');
+    print('fileName -> $fileName');
+    print('gistId -> $gistId');
+    print('timeAgo -> $timeAgo');
+    print('gistName -> $gistName');
+    print('filesCount -> $filesCount');
+    print('forksCount -> $forksCount');
+    print('commentsCount -> $commentsCount');
+    print('starsCount -> $starsCount');
+
+    ///
+    /// file-box
+    ///
+    final fileBoxElement = snippetElement.querySelector('div.file-box');
+    final codeTableRowElements = fileBoxElement
+        .querySelectorAll('table.js-file-line-container > tbody > tr');
+    for (final trElement in codeTableRowElements) {
+      final lineNumber = trElement.getElementsByTagName('td').first.attributes['data-line-number'];
+      final codeLine = trElement.getElementsByTagName('td').last.text.trim();
+      print('$lineNumber  $codeLine');
+    }
+  }
 }
